5e45111e97c3a3ac510ce07e0534971b78e53740
everton-da-costa <everto.cost@gmail.com>
2025-10-23 15:28:11 -0300

    fix: remove doMC dependency

--- R/BarmaRidgeBJPS2025-package.R
+++ R/BarmaRidgeBJPS2025-package.R
@@ -6,9 +6,7 @@
 #' @importFrom gridExtra grid.arrange
 #' @importFrom Rdpack reprompt
 #' @importFrom zoo as.yearmon
-#' @importFrom doMC registerDoMC
 #' @importFrom foreach %dopar% foreach
-#' @importFrom doRNG %dorng%
 
 ## usethis namespace: start
 #' @importFrom stats dbeta fitted frequency is.ts lm.fit make.link pchisq pnorm rbeta residuals start ts var
